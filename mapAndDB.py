e60f78cc087f7252ca6b86a23009d46e60a3b0f4
markus kaukonen <kaukonen.markus@gmail.com>
2018-02-09 13:34:36 +0200

    heroku bugfix

--- mapAndDB.py
+++ mapAndDB.py
@@ -12,10 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'porignvbpaiyhrvb98975sfur8'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Track, '/track/<string:name>')
